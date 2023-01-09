07e921173f098870867def23c9038f4a31d1693a
redanovich6 <48262256+redanovich6@users.noreply.github.com>
2023-01-09 11:51:52 +0000

    Create minishift.sh

---
+++ minishift.sh
@@ -0,0 +1,16 @@
+sudo adduser reda
+sudo usermod -aG sudo reda
+id reda
+su - reda
+sudo apt update
+sudo apt install qemu-kvm libvirt-daemon libvirt-daemon-system curl -y
+sudo usermod -a -G libvirt $(whoami)
+newgrp libvirt
+sudo curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-ubuntu14.04 -o /usr/local/bin/docker-machine-driver-kvm
+sudo chmod +x /usr/local/bin/docker-machine-driver-kvm
+systemctl is-active libvirtd
+sudo virsh net-list --all
+sudo mkdir /minioc && sudo chmod 777 /minioc && cd /minioc
+wget -qO- https://github.com/minishift/minishift/releases/download/v1.34.3/minishift-1.34.3-linux-amd64.tgz | tar xvz
+export PATH=/minioc/minishift-1.34.3-linux-amd64:$PATH
+minishift start
